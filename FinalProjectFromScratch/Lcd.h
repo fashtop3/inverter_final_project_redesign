4343904ac2422aca74218c4321d610709a9b7914
Fashola Ayodeji <fashtop3@gmail.com>
2016-11-01 16:27:42 +0100

    added lcd class

--- FinalProjectFromScratch/Lcd.h
+++ FinalProjectFromScratch/Lcd.h
@@ -9,19 +9,44 @@
 #ifndef __LCD_H__
 #define __LCD_H__
 
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define RS						PINB0
+#define RW						PINB1
+#define ENABLE					PINB2
+
+#define LCDsControl				PORTB
+#define LCDsControlDirection	DDRB
+
+#define LCDsCrib				PORTC
+#define LCDsCribDirection		DDRC
+
 
 class Lcd
 {
 //variables
 public:
-protected:
-private:
-
-//functions
-public:
 	Lcd();
 	~Lcd();
+	
+	Lcd* send_A_Command(unsigned char command);
+	Lcd* send_A_Character(unsigned char character);
+	Lcd* send_A_String(char *str);
+	Lcd* printStringToLCD(uint8_t x, uint8_t y, char *str);
+	Lcd* printIntToLCD(uint8_t x, uint8_t y, int val, uint8_t len);
+	Lcd* printDoubleToLCD(uint8_t x, uint8_t y, double val, uint8_t len, char prec);
+	Lcd* gotoLocation(uint8_t x, uint8_t y);
+	Lcd* clScr();
+	
+	Lcd* check_If_Busy();
+	Lcd* peek_A_Boo(void);
+
 protected:
+	int firstColumnPositions[2] = {0, 64};
+		
 private:
 	Lcd( const Lcd &c );
 	Lcd& operator=( const Lcd &c );
