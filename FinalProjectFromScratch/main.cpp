9b3fed355798ff327a4b20655f7b10e88d9818ee
Fashola Ayodeji <fashtop3@gmail.com>
2016-11-01 10:51:51 +0100

    refactor ISR analog pin switching

--- FinalProjectFromScratch/main.cpp
+++ FinalProjectFromScratch/main.cpp
@@ -12,12 +12,16 @@
 #include "Inverter.h"
 #include "serial.h"
 
+Inverter inverter(); //initialize inverter
 
 //function prototype
-void TIMER_setup();
+void my_timer_setup();
 
 int main(void)
 {
+	cli(); //clear global interrupt
+	sei(); //Enable Global Interrupt
+	my_timer_setup();
 	
     /* Replace with your application code */
     while (1) 
@@ -46,38 +50,41 @@ ISR(TIMER1_COMPA_vect)
 }
 
 
-//ISR(ADC_vect)
-//{
-	//switch(ADMUX)
-	//{
-		//case 0x60:
-		////conversion for AC input
+ISR(ADC_vect)
+{
+	switch(ADMUX)
+	{
+		case 0x60: //check the pin on Analog Multiplexer Pin layout
+		//conversion for AC input
 		//ad_ac_readings = ADCH;
-		//ADMUX = 0x61;
-		//break;
-		//case 0x61:
-		////Conversion for Battery level
+			inverter.setAcAnalogValue(ADCH);
+			ADMUX = 0x61; //set to enable next pin for analog conversion
+		break;
+		case 0x61:
+		//Conversion for Battery level
 		//ad_batt_readings = ADCH;
-		//ADMUX = 0x62;
-		//break;
-		//case 0x62:
-		////conversion for Overload
+			inverter.setBattAnalogValue(ADCH);
+			ADMUX = 0x62;  //set to enable next pin for analog conversion
+		break;
+		case 0x62:
+		//conversion for Overload
 		//ad_overload_readings = ADCH;
-		//ADMUX = 0x60;
-		//break;
-		//
-		//default:
-		//// default
-		//
-		//break;
-		//
-	//}
-	//
-	////start new conversion
-	//ADCSRA |= 1<<ADSC;
-//}
+			inverter.setOverloadAnalogValue(ADCH);
+			ADMUX = 0x60;  //set to enable next pin for analog conversion
+		break;
+		
+		default:
+		// default
+			ADMUX = 0x60; //set to first 
+		break;
+		
+	}
+	
+	//start new conversion
+	ADCSRA |= 1<<ADSC;
+}
 
-void TIMER_setup()
+void my_timer_setup()
 {
 	//1000000/64 = 15625
 	TCCR1B |= (1<<WGM12) | (1<<CS11) | (1<<CS10);
