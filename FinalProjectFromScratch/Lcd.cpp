590ca07e3652364ea0e963bd5f5523f220969384
Fashola Ayodeji <fashtop3@gmail.com>
2016-11-01 12:29:18 +0100

    create lcd class

---
+++ FinalProjectFromScratch/Lcd.cpp
@@ -0,0 +1,19 @@
+/* 
+* Lcd.cpp
+*
+* Created: 11/1/2016 12:26:32 PM
+* Author: Ayodeji
+*/
+
+
+#include "Lcd.h"
+
+// default constructor
+Lcd::Lcd()
+{
+} //Lcd
+
+// default destructor
+Lcd::~Lcd()
+{
+} //~Lcd
---
+++ FinalProjectFromScratch/Lcd.h
@@ -0,0 +1,31 @@
+/* 
+* Lcd.h
+*
+* Created: 11/1/2016 12:26:32 PM
+* Author: Ayodeji
+*/
+
+
+#ifndef __LCD_H__
+#define __LCD_H__
+
+
+class Lcd
+{
+//variables
+public:
+protected:
+private:
+
+//functions
+public:
+	Lcd();
+	~Lcd();
+protected:
+private:
+	Lcd( const Lcd &c );
+	Lcd& operator=( const Lcd &c );
+
+}; //Lcd
+
+#endif //__LCD_H__
