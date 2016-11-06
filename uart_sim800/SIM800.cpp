75850d16cd0f0314e324fe78562e34f9a27a3a5e
Fashola Ayodeji <fashtop3@gmail.com>
2016-11-06 22:49:59 +0100

    added SIM800 class

---
+++ uart_sim800/SIM800.cpp
@@ -0,0 +1,19 @@
+/* 
+* SIM800.cpp
+*
+* Created: 11/3/2016 7:06:36 PM
+* Author: Ayodeji
+*/
+
+
+#include "SIM800.h"
+
+// default constructor
+SIM800::SIM800()
+{
+} //SIM800
+
+// default destructor
+SIM800::~SIM800()
+{
+} //~SIM800
